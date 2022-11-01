d2fd29c6846c577c418a53109e71917f20fb0332
Mahmoud Khalid <darkness20016@gmail.com>
2022-11-01 18:23:20 +0200

    Upgrade utils

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,15 +1,12 @@
 import 'package:path/path.dart' as pathlib;
 
-Future<String> pathWithDate(String path) {
-  return Future(() {
-    return pathWithDateSync(path);
-  });
-}
-
-String pathWithDateSync(String path) {
-  String date = DateTime.now().toString().split('.')[0].replaceAll(':', '-');
-  String dirname = pathlib.dirname(path);
-  List<String> basename = pathlib.basename(path).split('.');
+String pathWithDate(String path) {
+  final String date = DateTime.now().toString().split('.')[0].replaceAll(
+        ':',
+        '-',
+      );
+  final String dirname = pathlib.dirname(path);
+  final List<String> basename = pathlib.basename(path).split('.');
   basename[0] = '${basename[0]} $date';
 
   return pathlib.prettyUri(
