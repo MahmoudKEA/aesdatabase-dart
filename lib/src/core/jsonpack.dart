16c883c54c80eb1258d312cf1a20cfbdf78a49bf
Mahmoud Khalid <darkness20016@gmail.com>
2022-12-03 18:40:21 +0200

    json encoder update

--- lib/src/core/jsonpack.dart
+++ lib/src/core/jsonpack.dart
@@ -2,15 +2,9 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 Uint8List jsonEncodeToBytes(Object data) {
-  return Uint8List.fromList(
-    utf8.encode(
-      jsonEncode(data),
-    ),
-  );
+  return utf8.encoder.convert(jsonEncode(data));
 }
 
 dynamic jsonDecodeFromBytes(Uint8List data) {
-  return jsonDecode(
-    utf8.decode(data),
-  );
+  return jsonDecode(utf8.decoder.convert(data));
 }
